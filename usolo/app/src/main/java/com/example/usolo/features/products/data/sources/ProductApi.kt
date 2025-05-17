d68b16125d63a0f772a2d1f6c8cfe7c8cbf12931
SebasL07 <sebasgnv0207@gmail.com>
2025-05-17 16:52:52 -0500

    feat: add products service

--- usolo/app/src/main/java/com/example/usolo/features/products/data/sources/ProductApi.kt
+++ usolo/app/src/main/java/com/example/usolo/features/products/data/sources/ProductApi.kt
@@ -6,9 +6,7 @@ import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.PATCH
-import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface ProductApi {
