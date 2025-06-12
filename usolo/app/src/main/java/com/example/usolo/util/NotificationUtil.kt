f80e3c8a3b7764d787d06c23d8a43e15d15754fb
Daniel Mejia <danielmax547@gmail.com>
2025-06-12 13:00:17 -0500

    style: fancy notification

--- usolo/app/src/main/java/com/example/usolo/util/NotificationUtil.kt
+++ usolo/app/src/main/java/com/example/usolo/util/NotificationUtil.kt
@@ -6,7 +6,9 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Intent
+import android.graphics.BitmapFactory
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import com.example.usolo.MainActivity
 import com.example.usolo.R
 
@@ -39,16 +41,24 @@ object NotificationUtil {
 
         manager.notify(
             id++,
-            NotificationCompat
-                .Builder(context, CHANNEL_ID)
+            NotificationCompat.Builder(context, CHANNEL_ID)
                 .setDefaults(Notification.DEFAULT_ALL)
-                .setContentText(message)
+                .setSmallIcon(R.drawable.ic_small_u)
+                .setLargeIcon(BitmapFactory.decodeResource(context.resources, R.drawable.ic_orangeusolo))
                 .setContentTitle(title)
+                .setContentText(message)
+                .setStyle(
+                    NotificationCompat.BigTextStyle()
+                        .bigText(message)
+                        .setBigContentTitle(title)
+                        .setSummaryText("Usolo")
+                )
                 .setContentIntent(notifyPendingIntent)
                 .setAutoCancel(true)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setColor(ContextCompat.getColor(context, R.color.primary_orange))
                 .build()
         )
+
     }
 
 }
